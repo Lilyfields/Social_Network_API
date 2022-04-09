aabdd8339e508784d1b7d178d903aca6e89483f3
Lilyfields <lilyfields11@bigpond.com>
2022-04-09 22:05:09 +1000

    models

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
+const User = require("../Models/user");
 
-router.get("/",(_req,res)=>{
-    res.send("user auth")
-})
+
+//Register
+router.get("/register",async(_req,_res)=>{
+    const user = await new User({
+        username:"Jane",
+        email:"jane@testmail.com",
+        password:"123456"
+    })
+
+    await user.save();
+    res.send("okay")
+
+});
+    
+
 
 
 module.exports= router
